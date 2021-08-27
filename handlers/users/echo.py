0ca4d840fbd1114b5d88c466b4337fc55a33ab1b
ArcNoah <Jack.gigant@gmail.com>
2021-08-27 16:21:40 +0600

    Состояние добавил, и переделал эхо хендлер, ну и админки закинул

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,20 +2,36 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from loader import dp
-
+from states.SomeState import meanless
+import asyncio
 
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
+    user_id = message.from_user.id
+    await dp.bot.send_message(340981880, f'Тут чорт написал, его Айди : {user_id}')
+    await dp.bot.send_message(340981880, f'Тут чорт написал, его сообщение : \n {message.text}')
+    if message.text == 'Я состоятельный' and meanless.statecounter == 0:
+        await message.answer('Похуй, будешь состоятельным хуем.')
+        await meanless.kekstate.set()
+        meanless.statecounter += 1
+    elif message.text == 'Я состоятельный' and meanless.statecounter != 0:
+        await message.answer('Нахуй иди, не состоятельный ты')   
+    else:
+        await message.answer(f"Ты не состоятельный Хуй."
+                         f"Твой сблев:\n"
                          f"{message.text}")
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
+    await message.answer("Ты конечно теперь состоятельный хуй,но мне поебать.")
+    await asyncio.sleep(3)
+    await message.answer('А вообще пошел нахуй,я забираю твое состояние')
+    user_id = message.from_user.id
+    await dp.bot.send_message(340981880, f'Тут чорт написал, его Айди : {user_id}')
+    await state.finish()
+    
+    
+
--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,8 +6,8 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Список команд: ",
-            "/start - Начать диалог",
-            "/help - Получить справку")
-    
+    text = ("На, только отъебись уже: ",
+            "/start - Пользуешься правами бога? Пидор ебаный.",
+            "/help - Не жди здесь помощи.")
+
     await message.answer("\n".join(text))
